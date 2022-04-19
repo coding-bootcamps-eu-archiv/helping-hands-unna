618368b61f12405de5da74c1356b0c0c89ef5436
Tobias Sindek <tobias.sindek@gmail.com>
2022-04-19 09:16:39 +0200

    refactor: change code to a mobile-first-approach
    The CSS targets smaller screen sizes first and will get media queries to change the layout to use the larger screen-real-estate. There is still some styling unset due to unfinished footer design

---
+++ script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+navLinks.forEach((navLink) =>
+  navLink.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
