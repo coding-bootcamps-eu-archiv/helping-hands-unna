a696c4b25e097eab153da9d194dc073b385d09b8
Tobias Sindek <tobias.sindek@gmail.com>
2022-04-21 19:41:08 +0200

    feat: add erreichtes website and table functunality

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-/****** nav bar  ******/
-
-=======
->>>>>>> 39bf799b9e844de2adb14f46181ce96aff784179
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLinks = document.querySelectorAll(".nav-link");
@@ -18,8 +13,51 @@ navLinks.forEach((navLink) =>
     navMenu.classList.remove("active");
   })
 );
-<<<<<<< HEAD
 
-/*** nav bar end ***/
-=======
->>>>>>> 39bf799b9e844de2adb14f46181ce96aff784179
+/**** table for bought items *****/
+
+let tabelle = document.querySelector("#tabelle");
+
+let td = document.createElement("td");
+
+function getData() {
+  fetch("http://ferdinand-niemann.eu:49179/items/produkte")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (dataObject) {
+      showData(dataObject);
+    });
+}
+
+function showData(dataObject) {
+  console.log(Object.keys(dataObject.data).length);
+  for (let i = 0; i < Object.keys(dataObject.data).length; i++) {
+    let itemRow = document.createElement("tr");
+    // Beschreibung
+    const beschreibung = document.createElement("td");
+    const description = dataObject.data[i].beschreibung;
+    beschreibung.innerText = description;
+
+    // Preis
+    const preis = document.createElement("td");
+    const price = dataObject.data[i].preis;
+    preis.innerText = price;
+
+    // Anzahl
+    const anzahl = document.createElement("td");
+    const quantity = dataObject.data[i].anzahl;
+    anzahl.innerText = quantity;
+    console.log(quantity);
+
+    // Datum
+    const datum = document.createElement("td");
+    const date = dataObject.data[i].datum;
+    datum.innerText = date;
+
+    itemRow.append(beschreibung, preis, anzahl, datum);
+    tabelle.appendChild(itemRow);
+  }
+}
+
+getData();
