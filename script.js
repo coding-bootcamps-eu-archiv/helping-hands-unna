55116c3a132c8268355bf1c3054f50ca26c0028b
Tobias Sindek <tobias.sindek@gmail.com>
2022-04-27 16:58:53 +0200

    feat: add 'aktuelle Spendenaktion' site with semi functional ticker
    current fundraiser sum connection to javascript still has to be implemented

--- script.js
+++ script.js
@@ -23,7 +23,11 @@ let td = document.createElement("td");
 function getData() {
   fetch("http://ferdinand-niemann.eu:49179/items/produkte")
     .then(function (res) {
-      return res.json();
+      if (res.ok) {
+        return res.json();
+      } else {
+        return;
+      }
     })
     .then(function (dataObject) {
       showData(dataObject);
@@ -31,33 +35,48 @@ function getData() {
 }
 
 function showData(dataObject) {
-  console.log(Object.keys(dataObject.data).length);
-  for (let i = 0; i < Object.keys(dataObject.data).length; i++) {
-    let itemRow = document.createElement("tr");
-    // Beschreibung
-    const beschreibung = document.createElement("td");
-    const description = dataObject.data[i].beschreibung;
-    beschreibung.innerText = description;
+  if (tabelle !== null) {
+    console.log(Object.keys(dataObject.data).length);
+    for (let i = 0; i < Object.keys(dataObject.data).length; i++) {
+      let itemRow = document.createElement("tr");
+      // Beschreibung
+      const beschreibung = document.createElement("td");
+      const description = dataObject.data[i].beschreibung;
+      beschreibung.innerText = description;
 
-    // Preis
-    const preis = document.createElement("td");
-    const price = dataObject.data[i].preis;
-    preis.innerText = price;
+      // Preis
+      const preis = document.createElement("td");
+      const price = dataObject.data[i].preis;
+      preis.innerText = price;
 
-    // Anzahl
-    const anzahl = document.createElement("td");
-    const quantity = dataObject.data[i].anzahl;
-    anzahl.innerText = quantity;
-    console.log(quantity);
+      // Anzahl
+      const anzahl = document.createElement("td");
+      const quantity = dataObject.data[i].anzahl;
+      anzahl.innerText = quantity;
+      console.log(quantity);
 
-    // Datum
-    const datum = document.createElement("td");
-    const date = dataObject.data[i].datum;
-    datum.innerText = date;
+      // Datum
+      const datum = document.createElement("td");
+      const date = dataObject.data[i].datum;
+      datum.innerText = date;
 
-    itemRow.append(beschreibung, preis, anzahl, datum);
-    tabelle.appendChild(itemRow);
+      itemRow.append(beschreibung, preis, anzahl, datum);
+      tabelle.appendChild(itemRow);
+    }
   }
 }
 
 getData();
+
+/**** fundraiser ticker data *****/
+
+const fundraiserTicker = document.querySelector(".cf-ticker");
+
+function changeCounterSum(newSum) {
+  if (fundraiserTicker !== null) {
+    const counterValue = Math.round((newSum / 60000) * 100);
+    fundraiserTicker.style.setProperty("--ticker-counter", counterValue + "%");
+  }
+}
+
+changeCounterSum(27000);
